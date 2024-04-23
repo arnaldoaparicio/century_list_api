48c7064606a2e80d6f23431c102f15d1bc2604ac
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-23 19:58:44 -0400

    Add 'show' method

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::ListsController < ApplicationController
     def index
         render json: List.all
     end
+
+    def show
+        render json: List.find(params[:id])
+    end
 end

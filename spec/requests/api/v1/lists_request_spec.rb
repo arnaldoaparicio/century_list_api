4dc1fe83e28585c7aa46dfa35daebb9ac3cfabe9
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-23 19:41:15 -0400

    Add tests to check keys

--- spec/requests/api/v1/lists_request_spec.rb
+++ spec/requests/api/v1/lists_request_spec.rb
@@ -8,5 +8,17 @@ describe 'Lists API' do
         get '/api/v1/lists'
 
         expect(response).to be_successful
+
+        lists = JSON.parse(response.body, symbolize_names: true)
+
+        expect(lists.count).to eq(2)
+
+        lists.each do |list|
+            expect(list).to have_key(:id)
+            expect(list[:id]).to be_an Integer
+
+            expect(list).to have_key(:name)
+            expect(list[:name]).to be_a String
+        end
     end
 end

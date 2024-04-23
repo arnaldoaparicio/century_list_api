626c95b0fab4ef116bc8439f5124a58f5b9f25f1
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-23 19:30:03 -0400

    Fix test

--- spec/requests/api/v1/lists_request_spec.rb
+++ spec/requests/api/v1/lists_request_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Lists API' do
     it 'sends a group of lists' do
-        list1 = List.new('Ingredients')
-        list2 = List.new('Paper')
+        list1 = List.create!({name: 'Ingredients'})
+        list2 = List.create!({name: 'Paper'})
 
         get '/api/v1/lists'
 

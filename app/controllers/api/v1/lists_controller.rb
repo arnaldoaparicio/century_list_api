edc6125d7b521056764e2a14089dd2d36fead7a2
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-23 20:56:17 -0400

    Add 'create' method and add strong params

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -7,4 +7,14 @@ class Api::V1::ListsController < ApplicationController
     def show
         render json: List.find(params[:id])
     end
+
+    def create
+        render json: List.create(list_params)
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:name)
+    end
 end

f3a340e18dbdb80d2dba9ea258530a219aff3c1c
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-25 09:34:32 -0400

    Adjust formatting

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,28 +1,27 @@
 class Api::V1::ListsController < ApplicationController
+  def index
+    render json: List.all
+  end
 
-    def index
-        render json: List.all
-    end
+  def show
+    render json: List.find(params[:id])
+  end
 
-    def show
-        render json: List.find(params[:id])
-    end
+  def create
+    render json: List.create(list_params)
+  end
 
-    def create
-        render json: List.create(list_params)
-    end
+  def update
+    render json: List.update(params[:id], list_params)
+  end
 
-    def update
-        render json: List.update(params[:id], list_params)
-    end
+  def destroy
+    render json: List.delete(params[:id])
+  end
 
-    def destroy
-        render json: List.delete(params[:id])
-    end
+  private
 
-    private
-
-    def list_params
-        params.require(:list).permit(:name)
-    end
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end

9a2dbac8cb07107a90fd0e8f0bda33bbcbb4bd38
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-25 09:32:36 -0400

    Adjust formatting

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe List, type: :model do
-    describe 'validations' do
-        it { should validate_presence_of(:name) }
-    end
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
 end

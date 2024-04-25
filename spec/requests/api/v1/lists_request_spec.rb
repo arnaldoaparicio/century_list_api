1f2e38bb5fd291fc7be6a071b94125a829e2fb87
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-04-25 09:33:38 -0400

    Adjust formatting

--- spec/requests/api/v1/lists_request_spec.rb
+++ spec/requests/api/v1/lists_request_spec.rb
@@ -1,78 +1,78 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'Lists API' do
-    it 'sends a group of lists' do
-        list1 = List.create!({name: 'Ingredients'})
-        list2 = List.create!({name: 'Paper'})
+describe "Lists API" do
+  it "sends a group of lists" do
+    list1 = List.create!({ name: "Ingredients" })
+    list2 = List.create!({ name: "Paper" })
 
-        get '/api/v1/lists'
+    get "/api/v1/lists"
 
-        expect(response).to be_successful
+    expect(response).to be_successful
 
-        lists = JSON.parse(response.body, symbolize_names: true)
+    lists = JSON.parse(response.body, symbolize_names: true)
 
-        expect(lists.count).to eq(2)
+    expect(lists.count).to eq(2)
 
-        lists.each do |list|
-            expect(list).to have_key(:id)
-            expect(list[:id]).to be_an Integer
+    lists.each do |list|
+      expect(list).to have_key(:id)
+      expect(list[:id]).to be_an Integer
 
-            expect(list).to have_key(:name)
-            expect(list[:name]).to be_a String
-        end
+      expect(list).to have_key(:name)
+      expect(list[:name]).to be_a String
     end
+  end
 
-    it 'can get one list by its id' do
-        list1 = List.create!({name: 'Ingredients'})
+  it "can get one list by its id" do
+    list1 = List.create!({ name: "Ingredients" })
 
-        get "/api/v1/lists/#{list1.id}"
+    get "/api/v1/lists/#{list1.id}"
 
-        list = JSON.parse(response.body, symbolize_names: true)
+    list = JSON.parse(response.body, symbolize_names: true)
 
-        expect(response).to be_successful
+    expect(response).to be_successful
 
-        expect(list).to have_key(:id)
-        expect(list[:id]).to eq(list1.id)
+    expect(list).to have_key(:id)
+    expect(list[:id]).to eq(list1.id)
 
-        expect(list).to have_key(:name)
-        expect(list[:name]).to eq('Ingredients')
-    end
+    expect(list).to have_key(:name)
+    expect(list[:name]).to eq("Ingredients")
+  end
 
-    it 'can create a new list' do
-        list_params = ({name: 'Ingredients'})
+  it "can create a new list" do
+    list_params = ({ name: "Ingredients" })
 
-        headers = {"CONTENT_TYPE" => "application/json"}
+    headers = { "CONTENT_TYPE" => "application/json" }
 
-        post "/api/v1/lists", headers: headers, params: JSON.generate(list: list_params)
-        created_list = List.last
+    post "/api/v1/lists", headers: headers, params: JSON.generate(list: list_params)
+    created_list = List.last
 
-        expect(response).to be_successful
-        expect(created_list.name).to eq(list_params[:name])
-    end
+    expect(response).to be_successful
+    expect(created_list.name).to eq(list_params[:name])
+  end
 
-    it 'can update an existing list' do
-        list1 = List.create!({name: 'Ingredients'})
-        list_params = { name: 'Food Product' }
-        headers = {"CONTENT_TYPE" => "application/json"}
+  it "can update an existing list" do
+    list1 = List.create!({ name: "Ingredients" })
+    list_params = { name: "Food Product" }
+    headers = { "CONTENT_TYPE" => "application/json" }
 
-        patch "/api/v1/lists/#{list1.id}", headers: headers, params: JSON.generate({list: list_params})
+    patch "/api/v1/lists/#{list1.id}", headers: headers, params: JSON.generate({ list: list_params })
 
-        list = List.find_by(id: list1.id)
+    list = List.find_by(id: list1.id)
 
-        expect(response).to be_successful
-        expect(list.name).to_not eq('Ingredients')
-        expect(list.name).to eq('Food Product')
-    end
+    expect(response).to be_successful
+    expect(list.name).to_not eq("Ingredients")
+    expect(list.name).to eq("Food Product")
+  end
 
-    it 'can destroy a list' do
-        list1 = List.create!({name: 'Ingredients'})
+  it "can destroy a list" do
+    list1 = List.create!({ name: "Ingredients" })
 
-        expect(List.count).to eq(1)
+    expect(List.count).to eq(1)
 
-        delete "/api/v1/lists/#{list1.id}"
+    delete "/api/v1/lists/#{list1.id}"
 
-        expect(response).to be_successful
-        expect(List.count).to eq(0)
-        expect{List.find(list1.id)}.to raise_error(ActiveRecord::RecordNotFound)
-    end
-end 
+    expect(response).to be_successful
+    expect(List.count).to eq(0)
+    expect { List.find(list1.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+end

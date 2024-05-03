0916dc68d027a41fa32ffbbb2385e4b251c9407e
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-05-03 17:12:44 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+ingredient_list = List.create!(name: 'Ingredients')
+paper_list = List.create!(name: 'Paper')

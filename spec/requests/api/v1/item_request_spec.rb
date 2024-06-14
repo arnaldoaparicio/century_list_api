91058fe582cdf68fb91db420e841fe334930f305
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-06-14 16:59:10 -0400

    Add test to check for Item details

---
+++ spec/requests/api/v1/item_request_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe "Item API" do
+  it "gets ingredient by its id number" do
+    list1 = List.create!({ name: "Ingredients" })
+
+    item1 = Item.create!({ name: "Baker's Cheese",
+                           minimum: 4,
+                           number_in_stock: 4,
+                           type: 0,
+                           list_id: "#{list1.id}" })
+    
+    get "/api/v1/lists/#{list1.id}/#{item1}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item[:data]).to have_key(:id)
+
+    expect(item[:data]).to have_key(:attributes)
+    
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to eq("Baker's Cheese")
+
+    expect(item[:data][:attributes]).to have_key(:minimum)
+    expect(item[:data][:attributes][:minumum]).to eq(4)
+
+    expect(item[:data][:attributes]).to have_key(:number_in_stock)
+    expect(item[:data][:attributes][:number_in_stock]).to eq(4)
+
+    expect(item[:data][:attributes]).to have_key(:type)
+    expect(item[:data][:attributes][:type]).to eq(0)
+    
+    
+  end
+end

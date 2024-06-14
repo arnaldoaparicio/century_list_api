7ba51811689acbb47cceb31c7bf7371fd551bbee
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-06-14 17:31:49 -0400

    Fix test

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe List, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
-    it { should have_many(:item)}
+    it { should have_many(:items)}
   end
 end

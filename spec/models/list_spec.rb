289f9d28673e3db04282b240974d54546af2ee57
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-06-14 17:04:40 -0400

    Add test to check 'has_many' association with 'Item'

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe List, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
+    it { should have_many(:item)}
   end
 end

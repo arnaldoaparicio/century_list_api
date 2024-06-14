78baeeb72d9039638ab782fd38507880dfb1e052
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-06-14 16:58:19 -0400

    Change schema to add one-to-many association with 'List' and 'Item'

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_23_232235) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_14_204420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "minimum"
+    t.integer "number_in_stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"

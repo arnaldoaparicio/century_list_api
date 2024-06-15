9b9ca0d9581ceb3d24258e3d4bb99d0974afd779
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-06-15 11:32:45 -0400

    Modify 'show' method to include Item serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
 
   def show
+    item = Item.find_by(id: params[:item_id])
+    render json: ItemSerializer.new(item), status: 200
   end
 end

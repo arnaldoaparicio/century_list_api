d1fb9ffd96039b48a018a27fd9b94fbbcaf4cc50
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-07-01 17:35:38 -0400

    Add validations to Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
+    validates_presence_of :name
+    validates_presence_of :minimum
+    validates_presence_of :number_in_stock
+    validates_presence_of :item_type
+    
     enum :item_type, [ :ingredient, :packaging, :cleaning]
-
+    
     belongs_to :list
 end

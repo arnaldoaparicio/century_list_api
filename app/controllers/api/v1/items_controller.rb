0a96c45a2f3fbce1af0c408ab5a01e73f96f0162
Arnaldo Aparicio <apaarn2211@gmail.com>
2024-08-11 09:14:36 -0400

    Add 'index' method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ItemsController < ApplicationController
 
+  def index
+    render json: ItemSerializer.new(Item.all), status: 200
+  end
+
   def show
     item = Item.find_by(id: params[:item_id])
     render json: ItemSerializer.new(item), status: 200
